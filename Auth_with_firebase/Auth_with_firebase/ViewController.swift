ec8620774c1a17e598d7b23524bfe3e3d4f0834e
Macostik <y.granchenko@gmail.com>
2020-05-14 12:33:16 +0300

    add project

---
+++ Auth_with_firebase/Auth_with_firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auth_with_firebase
+//
+//  Created by Maxim Granchenko on 14.05.2020.
+//  Copyright © 2020 Maxim Granchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
